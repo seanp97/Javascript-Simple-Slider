ef9b77861a7fcddde3dd8ab907a918195de02834
seanp97 <sean.pelser97@gmail.com>
2022-12-08 14:51:00 +0000

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,46 @@
+class Slider {
+
+    constructor(wrapper) {
+        this._wrapper = wrapper;
+        this.el = document.querySelector(this._wrapper);
+        this.sliderInit = 0;
+        this.allImages = document.querySelectorAll('.slider img');
+
+        this.allImages.forEach((image, index) => {
+            if(index != this.sliderInit - 1) {
+                this.initImage = document.querySelector('.slider img:first-of-type').src;
+                document.querySelector(`${this._wrapper} > img`).src = this.initImage;
+            }
+        
+            this.sliderIndex = index + 1;
+            image.classList.add(`slide-image-${this.sliderIndex}`)
+        });
+
+        console.log('Initialized');
+    }
+
+    PrevBtn(el) {
+        this._el = el;
+        document.querySelector(this._el).addEventListener('click', () => {
+            this.sliderInit--;
+            if(this.sliderInit === 0) {
+                this.sliderInit = this.allImages.length;
+            }
+            this.currentImage = document.querySelector(`.slide-image-${this.sliderInit}`).src;
+            document.querySelector(`${this._wrapper} > img`).src = this.currentImage;
+        })
+    }
+
+    NextBtn(el) {
+        this._el = el;
+        document.querySelector('#next').addEventListener('click', () => {
+            this.sliderInit++;
+            if(this.sliderInit > this.allImages.length) {
+                this.sliderInit = 1;
+            }
+            this.currentImage = document.querySelector(`.slide-image-${this.sliderInit}`).src;
+            document.querySelector(`${this._wrapper} > img`).src = this.currentImage;
+        })
+    }
+
+}
